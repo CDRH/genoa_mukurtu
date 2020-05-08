0293327fc648ea70593f8cf0b1dc9b7c5e34f564
Greg Tunink <techgique@gmail.com>
2020-05-08 18:14:01 -0500

    Re-build sub-theme and rename 'genoa'
    This was a result of investigating missing page elements whose cause
    couldn't be determined otherwise. This was accomplished by following the
    Drupal 7 guide:
    https://www.drupal.org/docs/7/theming/creating-a-sub-theme

--- js/collection-grid.js
+++
@@ -1,9 +0,0 @@
-jQuery(document).ready(function($){
-    // Grid Handling for collections
-    $(window).load(function() {
-	$('.field-name-field-digital-heritage-items').masonry({
-	    itemSelector: '.view-mode-search_result',
-	    gutter: 30,
-	});
-    });
-});
